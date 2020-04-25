2b68e06d62b2d06a25d439cdf93b6a9dbfe409a2
rennemannd <rennemannd@gmail.com>
2020-04-25 02:48:47 -0400

    Updates

---
+++ precache-manifest.d4f7fb3409485a86be929d6980a679c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7d7b83271a1866c185ec55405c0a3f",
+    "url": "/project-showcase/index.html"
+  },
+  {
+    "revision": "00544e94fc03dcf7d175",
+    "url": "/project-showcase/static/css/2.b328fd89.chunk.css"
+  },
+  {
+    "revision": "07ec14aa4cdeb6b6bdea",
+    "url": "/project-showcase/static/css/main.50abe98c.chunk.css"
+  },
+  {
+    "revision": "00544e94fc03dcf7d175",
+    "url": "/project-showcase/static/js/2.877d3b63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/project-showcase/static/js/2.877d3b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ec14aa4cdeb6b6bdea",
+    "url": "/project-showcase/static/js/main.24fd3e25.chunk.js"
+  },
+  {
+    "revision": "153cb8e05aac25e73797",
+    "url": "/project-showcase/static/js/runtime-main.139e3240.js"
+  }
+]);
