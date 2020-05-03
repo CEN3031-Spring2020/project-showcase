7a7e2fe7cfa968da1a48004a407f8a556b2b5333
Jonathan Conlin <jconlin@ufl.edu>
2020-05-03 11:57:20 -0400

    moved authors to footer

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import ProjectBox from '../components/ProjectBox/ProjectBox';
 import bestProjects from '../assets/bestProjects.json';
+import {ThemeContext} from "../App";
 
 function Home() {
-    return <ProjectBox bestProjects={bestProjects}/>
+    const scheme = useContext(ThemeContext);
+    return (
+        <>
+            <ProjectBox bestProjects={bestProjects}/>
+            <div className='text-center w-full font-normal p-5'>
+                     <span style={{fontSize: '12px', color: scheme.teamNameColor}}>
+                        Site created by: Jonathan Conlin, Dakota Rennemann, and Shaila Patel
+                     </span>
+            </div>
+        </>
+    )
 }
 
 export default Home;
